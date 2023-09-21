0e7ecfe33bc112c3503e04a94bedf688de1a0612
hanseol <145195969+aiot23@users.noreply.github.com>
2023-09-21 15:40:16 +0900

    MFCProjects_gLim

---
+++ mfcExam-Random circle/CDlgImage.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+
+
+// CDlgImage 대화 상자
+
+class CDlgImage : public CDialogEx
+{
+	DECLARE_DYNAMIC(CDlgImage)
+
+public:
+	CDlgImage(CWnd* pParent = nullptr);   // 표준 생성자입니다.
+	virtual ~CDlgImage();
+
+	CImage m_image;
+	CWnd* m_pParent;
+	CPoint m_ptData;
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DLGIMAGE };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedBtnUpParent();
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	int m_nNum = 0;
+
+
+private:
+	void InitImage();
+	void drawData(CDC* pDC);
+};
